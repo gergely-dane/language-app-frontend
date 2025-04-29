53beaeca8c94cd5d17a977bc33b5d89b87b09bd6
danegergo <danegergo19@gmail.com>
2025-04-29 23:48:35 +0300

    added shadcn, switched to yarn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <header className="flex flex-row justify-between mx-auto px-4 py-3 sm:px-6 bg-[var(--secondary)] w-full">
-        <a href="/">Home</a>
+      <header className="flex justify-center px-4 py-4 w-full sm:px-6 bg-[var(--secondary)]">
+        <Button>
+          <Link href="/">Home</Link>
+        </Button>
       </header>
     </div>
   );
