fa9adae7bc9418d8e0863056fb3ddc1eaf48cc2c
danegergo <danegergo19@gmail.com>
2025-05-08 13:22:19 +0300

    api changes

--- src/app/vocabulary/page.tsx
+++ src/app/vocabulary/page.tsx
@@ -3,16 +3,19 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useWords, Word } from "@/hooks/useWords";
+import {
+  useTranslations,
+  Tranlation,
+  TranslationsResponse,
+} from "@/hooks/useTranslations";
 
 export default function Home() {
-  const { data, isLoading, error } = useWords({});
+  const { data, isLoading, error } = useTranslations({});
   if (isLoading) return <div>Loading words...</div>;
   if (error) return <div>Error loading words</div>;
   if (!data) return <div>No words found</div>;
@@ -20,27 +23,31 @@ export default function Home() {
   return (
     <div className="flex justify-center w-full">
       <div className="w-[60%]">
+        <div className="text-3xl font-bold my-8">Word List</div>
         <Table>
-          <TableCaption>A list of your saved words.</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead>Word</TableHead>
-              <TableHead>Translation</TableHead>
+              <TableHead>Translation(s)</TableHead>
               <TableHead>Language</TableHead>
               <TableHead>Added on</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data.map((word: Word) => (
-              <TableRow key={word.id}>
-                <TableCell>{word.originalWord}</TableCell>
-                <TableCell>{word.translatedWord}</TableCell>
+            {data.map((tranlation: TranslationsResponse) => (
+              <TableRow key={tranlation.id}>
+                <TableCell>{tranlation.word}</TableCell>
                 <TableCell>
-                  {word.originalLanguageEnglish} {" → "}
-                  {word.translationLanguageEnglish}
+                  <span className="overflow-ellipsis overflow-hidden whitespace-nowrap w-[10px]">
+                    {tranlation.translations.join(", ")}
+                  </span>
                 </TableCell>
                 <TableCell>
-                  {new Date(word.createdDate).toLocaleDateString()}
+                  {tranlation.sourceLanguageCode} {" → "}
+                  {tranlation.translationLanguageCode}
+                </TableCell>
+                <TableCell>
+                  {new Date(tranlation.createdDate).toLocaleDateString()}
                 </TableCell>
               </TableRow>
             ))}
---
+++ src/hooks/useTranslations.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { apiClient } from "@/lib/apiClient";
+
+export interface Translation {
+  id: string;
+  word: string;
+  translations: string[];
+  sourceLanguageCode: string;
+  translationLanguageCode: string;
+  createdDate: string;
+}
+
+export interface TranslationsResponse extends Translation {}
+
+export interface TranslationsParams {}
+
+export const useTranslations = (params: TranslationsParams) => {
+  return useQuery({
+    queryKey: ["translations", params],
+    queryFn: async () => {
+      const { data } = await apiClient.get<TranslationsResponse[]>(
+        "/translations",
+        {
+          params,
+        },
+      );
+      return data;
+    },
+  });
+};
